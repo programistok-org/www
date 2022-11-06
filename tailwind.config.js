fb459ef37fbd4b1ddb93d4579d0763accd96b106
Maciej Korsan <maciejkorsan@gmail.com>
2022-11-07 00:28:25 +0100

    agenda + description + new layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        programistok: "#0098DA",
+        programistokDark: "#0077B5",
+      }
+    },
   },
   plugins: [],
 };
